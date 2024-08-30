9da028687480f94f0dd0ca506eb2820ab6486927
ciekotics <rahul.saxena@jabotics.in>
2024-08-30 14:09:31 +0530

    about us

--- src/components/live/home/about-us/about-details.tsx
+++ src/components/live/home/about-us/about-details.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import Separator from '@/components/utils/separator';
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react';
-import { FaRegPlayCircle } from 'react-icons/fa';
+import Separator from "@/components/utils/separator";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { FaRegPlayCircle } from "react-icons/fa";
 
 // CUSTOM IMPORTS
 
@@ -50,21 +50,20 @@ const AboutUsDetails = () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
-  
+
+  console.log(isPlaying);
   return (
     <div>
       <div className="container">
         <div className="about-content mt-20">
-          <h2 className="" id="about-label">
+          <h2 className="label" id="about-label">
             About Us
           </h2>
           <Separator />
-          <h3
-            className=" mt-10"
-          >
+          <h3 className="headline">
             Elevating Culinary Creations with the Finest Spices
           </h3>
-          <p className="text-base mt-10 leading-7 ">
+          <p className="description">
             Where the art of spice blending meets the science of quality and
             flavor. We pride ourselves on crafting the finest spices that add
             zest and flavor to kitchens around the globe. Quality is at the
@@ -72,9 +71,9 @@ const AboutUsDetails = () => {
             spices and experience the passion and dedication that goes into
             every product we create.
           </p>
-          <div className="mt-10 font-bold text-white text-lg">Contact On</div>
-          <a href="tel:+804001234567" className="text-2xl block mb-5">
-            +91 980 123 4567
+          <div className="mobile-text">Contact On :</div>
+          <a href="tel:+804001234567" className="mobile">
+            +91 980-123-4567
           </a>
         </div>
         <figure className="about-banner">
@@ -88,32 +87,46 @@ const AboutUsDetails = () => {
             data-parallax-item
             data-parallax-speed="1"
           />
-          {/* <div
-            className="abs-img abs-img-1 has-before"
+          <div
+            className="abs-img-1"
             data-parallax-item
             data-parallax-speed="1.75"
           >
-            <div className="h-60 rounded-md overflow-hidden relative">
+            <div className="content-wrapper">
               <video
                 ref={videoRef}
-                src={'/videos/about.mp4'}
+                src={"/videos/about.mp4"}
                 loop
                 muted
-                className="object-cover object-center h-full cursor-pointer"
+                // className="object-cover object-center h-full cursor-pointer"
                 onClick={togglePlay}
               ></video>
-              <div className={`absolute top-0 left-0 w-full h-full bg-stone-800 flex items-center justify-center ${isPlaying ? 'hidden' : 'inline-block'}`}>
-                <FaRegPlayCircle onClick={togglePlay} size={50} className="cursor-pointer" />
+              <div
+                className={`play-icon`}
+                style={{ display: isPlaying ? "none" : "" }}
+              >
+                <FaRegPlayCircle
+                  color="white"
+                  onClick={togglePlay}
+                  size={50}
+                  className="cursor-pointer"
+                />
               </div>
             </div>
-          </div> */}
-          <div className="abs-img abs-img-2 has-before">
-            <Image src={"/images/badge-2.png"} width="133" height="134" loading="lazy" alt="" />
+          </div>
+          <div className="abs-img ">
+            <Image
+              src={"/images/badge-2.png"}
+              width="133"
+              height="134"
+              loading="lazy"
+              alt=""
+            />
           </div>
         </figure>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUsDetails
+export default AboutUsDetails;
