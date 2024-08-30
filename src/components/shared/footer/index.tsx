7d1b363229a2033b0eb026a112a5d831f34affe2
ciekotics <rahul.saxena@jabotics.in>
2024-08-30 16:42:39 +0530

    footer

--- src/components/shared/footer/index.tsx
+++ src/components/shared/footer/index.tsx
@@ -1,12 +1,61 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <footer id='footer'>
-      <div className='footer-links'></div>
-      <div className='bottom-tabs'></div>
-    </footer>
-  )
-}
+    <footer id="footer">
+      <div className="footer-links">
+        <div className="left">
+          <Image
+            src={"/images/logo_black.svg"}
+            alt="logo-footer"
+            width={50}
+            height={50}
+            className="img"
+          />
 
-export default Footer
+          <p>
+            We pride ourselves on crafting the finest spices that add zest and
+            flavor to kitchens around the globe.
+          </p>
+        </div>
+        <div className="middle">
+          <p>Useful Links</p>
+
+          <div className="links">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/"}>Our Products</Link>
+            <Link href={"/blogs"}>Blogs</Link>
+            <Link href={"/"}>Contact Us</Link>
+            <Link href={"/"}>How It Works</Link>
+          </div>
+        </div>
+        <div className="right">
+          <p>Contact Us</p>
+
+          <div className="links">
+            <div className="mail">
+              <span className="icon">
+                <MdOutlineMailOutline size={20} />
+                <span>Mail Us :</span>
+              </span>
+              <Link href={"/"}>wardanspices@gmail.com</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-tabs">
+        <div className="w-1/2">Copyright &copy; 2024 wardan</div>
+        <div className="w-1/2 bottom-tabs__links">
+          <h5>Sitemap</h5>
+          <h5>Disclaimer</h5>
+          <h5>Privacy Policy</h5>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
