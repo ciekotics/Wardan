329dab454c12cd7d133b240a4ba10de0a28e9d71
ciekotics <rahul.saxena@jabotics.in>
2024-09-02 18:32:04 +0530

    strings length change

--- src/components/admin/add-blog/newblog-schema.ts
+++ src/components/admin/add-blog/newblog-schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const newBlogsSchema = z.object({
-  title: z.string().min(10, { message: 'Title is required' }).max(15, {  message: 'Enter a Valid Title' }),
-  description: z.string().min(15, { message: 'Enter a Valid Description' }).max(35, {  message: 'Enter a Valid Description' }).optional(),
-  smallParagraph: z.string().min(25, { message: 'Enter a Valid Paragraph' }).max(50, {  message: 'Enter a Valid Paragraph' }).optional(),
-  longParagraph: z.string().min(150, { message: 'Enter a Valid Text' }).max(200, {  message: 'Enter a Valid Text' }),
+  title: z.string().min(15, { message: 'Title is required' }).max(25, {  message: 'Enter a Valid Title' }),
+  description: z.string().min(45, { message: 'Enter a Valid Description' }).max(60, {  message: 'Enter a Valid Description' }).optional(),
+  smallParagraph: z.string().min(50, { message: 'Enter a Valid Paragraph' }).max(100, {  message: 'Enter a Valid Paragraph' }).optional(),
+  longParagraph: z.string().min(350, { message: 'Enter a Valid Text' }).max(800, {  message: 'Enter a Valid Text' }),
 })
