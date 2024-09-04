6f4a3aae907593ee18e1b2b8cb9befd5c8b30811
ciekotics <rahul.saxena@jabotics.in>
2024-09-04 11:59:01 +0530

    products carousel

--- src/components/live/home/hero/item-3/top-news-item.tsx
+++ src/components/live/home/hero/item-3/top-news-item.tsx
@@ -1,5 +1,6 @@
-import { useGetAllBlogsQuery } from '@/store/actions/slices/api-slice';
-import React from 'react'
+import { useGetAllBlogsQuery } from "@/store/actions/slices/api-slice";
+import Image from "next/image";
+import React from "react";
 
 const TopNewsItem = () => {
   const {
@@ -9,21 +10,35 @@ const TopNewsItem = () => {
     isError,
   } = useGetAllBlogsQuery({});
 
-  // if (isLoading) {
-  //   return <LoadingSkeleton />;
-  // }
-
   if (isError) {
     return <div>Error loading blogs</div>;
   }
-  return (
-    <div className='top-news__item'>
-      <div className='content'>
-      <p className="title loading"></p>
-      <p className="date loading"></p>
-      </div>
-    </div>
-  )
-}
 
-export default TopNewsItem
+  // Optional: Show loading state
+  if (isLoading) {
+    return (
+      <div className="top-news__item top-news__item-loading">
+        <div className="content">
+          <p className="title loading"></p>
+          <p className="date loading"></p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {allBlogs?.blogs?.map((item, index) => (
+        <div className="top-news__item" key={index}>
+          <Image src={item.banner} alt={item.title} layout="fill" objectFit="cover" />
+          <div className="content__loaded">
+            <p className="title">{item.title}</p>
+            <p className="date">{item["created-at"]}</p>
+          </div>
+        </div>
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default TopNewsItem;
